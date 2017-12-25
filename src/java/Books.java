49f4d8f2caf6188197de7f0f51d5d70c34d51c12
gitk47320 <34836412+gitk47320@users.noreply.github.com>
2017-12-25 23:05:02 +0900

    Initial commit

---
+++ src/java/Books.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author junya
+ */
+public class Books {
+    private int id;
+    private String name;
+    private String author;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }    
+}
---
+++ src/java/BooksBean.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author junya
+ */
+@ManagedBean
+@RequestScoped
+
+public class BooksBean implements Serializable{
+
+    /**
+     * Creates a new instance of BooksBean
+     */
+    
+    public BooksBean() {
+    }
+    
+    //DB接続し、その中身をListに保持する。
+    public ArrayList<Books> selectfrombooks() throws ClassNotFoundException, SQLException{
+      //oracleのライブラリを読み込む
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      //oracleのDBに接続する
+      Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@(接続先IPorホスト):1521:(oracle sid)","(ユーザID)","(パスワード)");
+      //oracleとのセッションを張る
+      Statement stmt = conn.createStatement();
+      //上記で接続したDBに対して、SQLを実行する。
+      //booksというテーブルから全データを取得している。
+      ResultSet rset = stmt.executeQuery("select * from books");
+      //取得したデータを格納するリスト
+      ArrayList<Books> booklist = new ArrayList<Books>();
+      //取得したデータをリストに格納する。
+      while(rset.next()){
+          Books books = new Books();
+          books.setId(rset.getInt("id"));
+          books.setName(rset.getString("name"));
+          books.setAuthor(rset.getString("author"));
+          //System.out.println(rset.getInt(1) + " " + rset.getString(2) + " " + rset.getString(3))
+          booklist.add(books);
+      }
+      //SQL送信処理を切断
+      rset.close();
+      //セッションを切断
+      stmt.close();
+      //DB接続を切断
+      conn.close();
+      return booklist;
+    }
+}
